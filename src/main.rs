7fa979d60fdf996e5e8f17f58430539d54ca7f7a
Andrew Dobrich <andrew.dobrich@protonmail.com>
2019-08-29 17:22:30 +1000

    Connect to a database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use log::{trace};
+
+mod database;
+
 fn main() {
-    println!("Hello, world!");
+    let conn = match database::SQLiteConnection::new()
+        .with_path("./db")
+        .connect()
+    {
+        Ok(conn) => {
+            trace!("Successfully connected to SQLite database.");
+            conn
+        },
+        Err(e) => {
+            panic!("No database connection available: {}", e);
+        }
+    };
 }
